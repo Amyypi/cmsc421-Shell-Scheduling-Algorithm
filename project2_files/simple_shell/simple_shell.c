5632343aeab5f85b9afa43f11dc5a8bdb2be8347
Amy Phan <mphan1@umbc.edu>
2022-03-10 05:11:52 -0500

    Most of part 1 done

--- project2_files/simple_shell/simple_shell.c
+++ project2_files/simple_shell/simple_shell.c
@@ -25,10 +25,13 @@ Project name: Project 2 part 1
 #define NUMBER_OF_BUILTINS = 1; //change if more builtins are added
 
 //List of functions//
+char **pars_with_quote(char *line, int num_spaces);
 char **pars(char *line);
 char *ask_cmd(void);
 
-//List of command builtin functions:
+///////////////////////////////////////////
+// List of command builtin functions:    //
+//////////////////////////////////////////
 int exit_cmd(char **args);
 
 char *builtin_cmd_list[] = {
@@ -48,19 +51,18 @@ int num_builtins(){
 int exit_cmd(char **args){
 	int arg_num = 0;
 	printf("exit_cmd called \n");
-	/*
+
 	// zero argument given
 	if(args[1] == NULL){
 		printf("No argument given in exit cmd");
-		return 0;
+		return (char *)0;
 	// one argument given
 	}else{
 		//parsing failed
 		if(arg_num == -1){
-			return 0;
-		{
+			return (char *)0;
+		}
 	}
-	*/
 	//command exit entered
 	return (char *)0;
 }
@@ -70,28 +72,42 @@ void shell(){
 	char **argument;
 	//char *argument_esc;
 	int status = 1;
+	int first_space;
+	int num_of_spaces;
 
 	do{
+
+		//ask for command from user
 		printf("$");
 		line = ask_cmd();
-		argument = pars(line);
 
-		printf("this is argument: %s \n", argument[0]);
+		//checks if there are any quotes in the command line
+		first_space = first_unquoted_space(line);
 
-		if(argument[0] == NULL){
-			printf("Empty argument");
-		/*
-		}else if(strcmp(argument[0], builtin_cmd_list[0])){
-			printf("exit called\n");
-			status = exit_cmd(argument);
-		*/
-		}else{
-			//argument_esc = unescape(argument[1], "unescape failed");
-			//printf("this is unescape %s\n",  argument_esc);
+		//if there is a quote, do parsing with quote
+		if(first_space >= 0){
+			num_of_spaces = count_spaces(line);
+			argument = pars_with_quote(line, num_of_spaces);
+
+			printf("argument with quote 1: %s\n", argument[0]);
+			printf("argument with quote 2: %s\n", argument[1]);
+
+			argument[1] = unescape(argument[1], "error");
+			printf("Print echo here: \n");
 			status = execute_cmd(argument);
+			free(argument[1]);
+			argument[1] = NULL;
+		//if there aren't any quotes, do regular parsing
+		}else{
+			argument = pars(line);
+			printf("this is argument: %s \n", argument[0]);
+			//check if input isn't empty;
+			if(argument[0] == NULL){
+				printf("Empty argument");
+			}else{
+				status = execute_cmd(argument);
+			}
 		}
-
-
 		free(line);
 		free(argument);
 	//	free(argument_esc);
@@ -99,7 +115,6 @@ void shell(){
 
 	line = NULL;
 	argument = NULL;
-//	argument_esc = NULL;
 
 	printf("\n Exit out");
 }
@@ -107,7 +122,6 @@ void shell(){
 int launch_cmd(char **args){
 	pid_t pid, wpid;
 	int counter = 0;
-//	int length = trlen(args);
 	int status;
 
 	printf("Before fork\n");
@@ -130,7 +144,6 @@ int launch_cmd(char **args){
 		wpid = waitpid(pid, &status, WUNTRACED);
 		}while(!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
-
 	return 1;
 }
 
@@ -152,6 +165,42 @@ int execute_cmd(char **args){
 
 	printf("launch called\n");
 	return launch_cmd(args);
+}
+
+char **pars_with_quote(char *line, int num_spaces){
+        printf("Enter pars with quote\n");
+        int bufsize = 64;
+        int position = 0;
+	int counter = 0;
+        char **tokens = malloc(bufsize *sizeof(char*));
+        char *token;
+
+        if(!tokens){
+                fprintf(stderr,"allocation error\n");
+                exit(EXIT_FAILURE);
+        }
+        token = strtok(line," ");
+	//tokens[position] = token;
+
+	
+        for(counter = 0; counter < num_spaces + 1; counter++){
+                tokens[position] = token;
+                position++;
+                if(position >= bufsize){
+                        bufsize += 64;
+                        tokens = realloc(tokens, bufsize * sizeof(char*));
+                        if(!tokens){
+                                fprintf(stderr, "allocation error\n");
+                                exit(EXIT_FAILURE);
+                        }
+                }
+                token = strtok(NULL,"\"");
+        }
+	
+
+        printf("Parsing with quote complete");
+        tokens[position];
+        return tokens;
 }
 
 
@@ -226,6 +275,5 @@ char *ask_cmd(void){
 int main(int argc, char **argv){
 	//start shell and ask user a command
         shell();
-
 	return EXIT_SUCCESS;
 }
