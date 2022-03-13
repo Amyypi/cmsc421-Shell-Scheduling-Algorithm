b1db6ed1e0929696b2290100ba54fafc1b44da7c
Amy Phan <mphan1@umbc.edu>
2022-03-13 18:53:00 -0400

    sjf done

--- project2_files/scheduling_algorithms/schedule_fcfs.c
+++ project2_files/scheduling_algorithms/schedule_fcfs.c
@@ -14,39 +14,26 @@ struct node *head;
 
 void add(char *name, int priority, int burst){
 
-//check task->name for insertion etc
-
-	//printf("add function executed\n");
+	// create a new task and put values into task
 	struct task *taskNode;
 	taskNode = malloc(sizeof(taskNode) * (5));
 	taskNode->name = name;
 	taskNode->tid = 0;
 	taskNode->priority = priority;
 	taskNode->burst = burst;
-	//printf("Successful new task added in\n");
 
-	//printf("make new task_list node\n");
-	//add task to the list
+	//create a new node, and put task in it
 	struct node *task_list = malloc(sizeof(struct node));
 	task_list->task = taskNode;
 	task_list->next = NULL;
-	//printf("Successful task_list node made\n");
 
 	struct node *temp = head;
 
-	//printf("Before adding node to head list\n");
-	//
+	// Add a new node to the linked list, if head == null, make the new node the head
 	if(head == NULL){
-		//printf("make new node head\n");
 		task_list->next = head;
 		head = task_list;
-		//head->next = NULL;
-		//printf("Successful head node\n");
 	}else{
-		//printf("Add node to the end of the list\n");
-		//struct node *temp;
-		//temp = head;
-
 		//traverse until last node and add node at end
 		while(temp->next != NULL){
 			temp = temp->next;
@@ -55,29 +42,14 @@ void add(char *name, int priority, int burst){
 		printf("[%s] [%d] [%d].\n",taskNode->name, taskNode->priority, taskNode->burst);
 		temp->next = task_list;
 
-		//printf("Successful node added at the end of list - add()\n");
-
 	}
-	//free(temp);
 	printf("\n");
 	return;
 }
 
 void schedule(){
 
-
 	printf("\n\n");
-	//do algorithm
-
-	// loop - make new list of ordered algorithm
-	// execute list.h
-	// void insert(struct node **head, Task *task);
-        	//add a new task to the list of tasks
-	// void delete(struct node **head, Task *task); 
-        	//delete the selected task from the list
-	// void traverse(struct node *head); 
-        	//traverse the list
-
 
 ///////////////////////////////////////
 
@@ -86,7 +58,7 @@ void schedule(){
         // pass in every node of the list until null
 	// Once done, free each node and task
 
-	// assign node list to this
+	// print out each task
 	struct node *temp;
 	temp = head;
 	printf("Print list of tasks\n");
@@ -95,6 +67,7 @@ void schedule(){
 		temp = temp->next;
 	}
 
+	// once done with printing, free all the nodes/tasks and exit
 	printf("Successful traverse list print, freeing node and exit\n");
 	struct task *task_temp;
 	temp = head;
--- project2_files/scheduling_algorithms/schedule_sjf.c
+++ project2_files/scheduling_algorithms/schedule_sjf.c
@@ -10,24 +10,114 @@
 
 // define MIN_PRIORITY 1
 // deifne MAX_PRIORITY 10
+struct node *head;
+
+void sortedInsert(struct node *newNode){
+	//struct node dummy;
+	struct node* current;
+	current = head;
+	//dummy.next = head;
+
+	if(current == NULL){
+		printf("current emtpy\n");
+	}else if(current->task->burst >  newNode->task->burst){
+		//burst is bigger than new node's burst
+
+		newNode->next = head;
+		head = newNode;
+
+
+	}else{
+		while(current->next != NULL && current->next->task->burst < newNode->task->burst){
+			current = current->next;
+		}
+		newNode->next = current->next;
+		current->next = newNode;
+		//head = dummy.next;
+	}
+}
+
 
 void add(char *name, int priority, int burst){
+        // create a new task and put values into task
+        struct task *taskNode;
+        taskNode = malloc(sizeof(taskNode) * (5));
+        taskNode->name = name;
+        taskNode->tid = 0;
+        taskNode->priority = priority;
+        taskNode->burst = burst;
 
+        //create a new node, and put task in it
+        struct node *task_list = malloc(sizeof(struct node));
+        task_list->task = taskNode;
+        task_list->next = NULL;
+
+        //struct node *temp = head;
+	struct node* result = NULL;
+	struct node* current = head;
+	struct node* next;
+
+        // Add a new node to the linked list, if head == null, make the new node the head
+        if(head == NULL){
+                task_list->next = head;
+                head = task_list;
+        }else{
+                //traverse until last node and add node at end
+                /*
+		while(temp->next != NULL){
+                        temp = temp->next;
+                }
+                printf("Inserted task, ");
+                printf("[%s] [%d] [%d].\n",taskNode->name, taskNode->priority, taskNode->burst);
+                temp->next = task_list;
+		*/
+
+		//while(current != NULL){
+
+		//	next = current->next;
+			sortedInsert(task_list);
+		//	current = next;
+
+		//}
+		//head = result;
+	}
+        printf("\n");
+        return;
 }
 
 void schedule(){
+        printf("\n\n");
+
+
+	//order based on cpu burst?
+
+
+///////////////////////////////////////
 
         // traverse and pass task of each node into run()
         // execute run(Task *task, int slice) from list
-        // pass in every node of the list until null?
+        // pass in every node of the list until null
+        // Once done, free each node and task
+
+        // print out each task
         struct node *temp;
         temp = head;
+        printf("Print list of tasks\n");
         while(temp != NULL){
-                //find out what slice does
-                run(temp->task, int slice) 
+                run(temp->task, 10);
                 temp = temp->next;
-
         }
 
+        // once done with printing, free all the nodes/tasks and exit
+        printf("Successful traverse list print, freeing node and exit\n");
+        struct task *task_temp;
+        temp = head;
+        while(head != NULL){
+                temp = head;
+                task_temp = temp->task;
+                head = head->next;
+                free(task_temp);
+                free(temp);
+        }
 
 }
